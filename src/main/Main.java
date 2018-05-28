eb88ed2d73e0d3005e4bae0f4a154ee75793b94f
muumiso <38120187+muumiso@users.noreply.github.com>
2018-05-28 13:31:39 +0900

    転記演習

---
+++ src/main/Main.java
@@ -0,0 +1,211 @@
+package main;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args)
+    {
+        InBean ib = new InBean();
+        ib.setTorihikisakiName("セントラル商会");
+        ib.setTorihikiDate( LocalDate.of(2018, 5, 2) );
+        ib.setGoukei(400);
+
+        ib.getLines().add(  new InLineBean("消しゴム",1,100) );
+        ib.getLines().add(  new InLineBean("鉛筆",20,300) );
+
+        OutBean ob = tenki( ib );
+        System.out.println( ob.toString() );
+    }
+
+    @Override
+    public String toString() {
+        return "Main []";
+    }
+
+    static OutBean tenki( InBean ib)
+    {
+        OutBean ob = new OutBean();
+
+//    	1. 日付を確認
+        LocalDate torihikiDate = ib.getTorihikiDate();
+//    	2. 日付を記入
+        ob.setTorihikiDate(torihikiDate);
+//    	3. 取引先を確認
+        String torihikisakiName = ib.getTorihikisakiName();
+//    	4. 取引先を記入
+        ob.setTorihikisakiName(torihikisakiName);
+
+//------------------------------------------------------------------
+//    	5. 5×4の表を作成
+//    	6. 列の項目名を１つ確認
+//    	　列の項目名を記入
+//    	　全ての項目について繰り返す
+//    	7. 行数を記入
+//------------------------------------------------------------------
+
+        for(InLineBean ilb : ib.getLines()) {
+            OutLineBean olb = new OutLineBean();
+//    	8. 商品名を確認
+            String shouhinName = ilb.getShouhinName();
+//    	9. 商品名を元の伝票と同じ位置に記入
+            olb.setShouhinName(shouhinName);
+//    	10. 数量、金額についても8,9と同様に行う
+            int suuryo = ilb.getSuuryo();
+            olb.setSuuryo(suuryo);
+            int kingaku = ilb.getKingaku();
+            olb.setKingaku(kingaku);
+
+            ob.getLines().add(olb);
+//    	11. 8,9,10を次の商品についても行う
+//    	12. 4行記入したら次のページに移る
+//    	13. １～12を伝票の終わりまで同様に行う
+        }
+//    	14. 合計金額を確認
+        int goukei = ib.getGoukei();
+//    	15. 合計金額を最後のページに記入する
+        ob.setGoukei(goukei);
+
+        return ob;
+    }
+
+}
+//------------------------------------
+class InBean
+{
+    private LocalDate 			torihikiDate;
+    private String	  			torihikisakiName;
+    private int		  			goukei;
+    private List<InLineBean> 	lines= new ArrayList<InLineBean>();
+
+    public LocalDate getTorihikiDate() {
+        return torihikiDate;
+    }
+    public void setTorihikiDate(LocalDate torihikiDate) {
+        this.torihikiDate = torihikiDate;
+    }
+    public String getTorihikisakiName() {
+        return torihikisakiName;
+    }
+    public void setTorihikisakiName(String torihikisakiName) {
+        this.torihikisakiName = torihikisakiName;
+    }
+    public int getGoukei() {
+        return goukei;
+    }
+    public void setGoukei(int goukei) {
+        this.goukei = goukei;
+    }
+    public List<InLineBean> getLines() {
+        return lines;
+    }
+
+
+
+}
+class InLineBean
+{
+    private String shouhinName;
+    private int		suuryo;
+    private int		kingaku;
+
+
+
+    public InLineBean(String shouhinName, int suuryo, int kingaku) {
+        super();
+        this.shouhinName = shouhinName;
+        this.suuryo = suuryo;
+        this.kingaku = kingaku;
+    }
+    public String getShouhinName() {
+        return shouhinName;
+    }
+    public void setShouhinName(String shouhinName) {
+        this.shouhinName = shouhinName;
+    }
+    public int getSuuryo() {
+        return suuryo;
+    }
+    public void setSuuryo(int suuryo) {
+        this.suuryo = suuryo;
+    }
+    public int getKingaku() {
+        return kingaku;
+    }
+    public void setKingaku(int kingaku) {
+        this.kingaku = kingaku;
+    }
+
+
+}
+//------------------------------------
+class OutBean
+{
+  private LocalDate 			torihikiDate;
+  private String	  			torihikisakiName;
+  private int		  			goukei;
+  private List<OutLineBean> 	lines= new ArrayList<OutLineBean>();
+
+    public LocalDate getTorihikiDate() {
+        return torihikiDate;
+    }
+    public void setTorihikiDate(LocalDate torihikiDate) {
+        this.torihikiDate = torihikiDate;
+    }
+    public String getTorihikisakiName() {
+        return torihikisakiName;
+    }
+    public void setTorihikisakiName(String torihikisakiName) {
+        this.torihikisakiName = torihikisakiName;
+    }
+    public int getGoukei() {
+        return goukei;
+    }
+    public void setGoukei(int goukei) {
+        this.goukei = goukei;
+    }
+    public List<OutLineBean> getLines() {
+        return lines;
+    }
+
+    @Override
+    public String toString() {
+        return "OutBean [torihikiDate=" + torihikiDate + ", torihikisakiName=" + torihikisakiName + ", goukei=" + goukei
+                + ", lines=" + lines + "]";
+    }
+
+}
+//------------------------------------
+class OutLineBean
+{
+  private String shouhinName;
+  private int		suuryo;
+  private int		kingaku;
+    public String getShouhinName() {
+        return shouhinName;
+    }
+    public void setShouhinName(String shouhinName) {
+        this.shouhinName = shouhinName;
+    }
+    public int getSuuryo() {
+        return suuryo;
+    }
+    public void setSuuryo(int suuryo) {
+        this.suuryo = suuryo;
+    }
+    public int getKingaku() {
+        return kingaku;
+    }
+    public void setKingaku(int kingaku) {
+        this.kingaku = kingaku;
+    }
+
+    @Override
+    public String toString() {
+        return "OutLineBean [shouhinName=" + shouhinName + ", suuryo=" + suuryo + ", kingaku=" + kingaku + "]";
+    }
+
+
+}
